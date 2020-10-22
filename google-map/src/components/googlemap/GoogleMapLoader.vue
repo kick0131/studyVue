46447d7eb5e3aa985f72d9a0b401c5de193cf858
HiramatsuChihiro <hiramatsu.chr@ncontr.com>
2020-10-22 17:04:04 +0900

    googlemapにアイコン導入ソース追加

--- google-map/src/components/googlemap/GoogleMapLoader.vue
+++ google-map/src/components/googlemap/GoogleMapLoader.vue
@@ -46,16 +46,25 @@ export default {
       // The marker, positioned at Uluru
       this.marker = new this.google.maps.Marker({
         position: this.mapConfig.center,
+        clickable: true,
         map: this.map,
+        icon: "pin_white.png",
         // icon: {
-        //   fillColor: "#FF0000", //塗り潰し色
-        //   fillOpacity: 0.8, //塗り潰し透過率
-        //   path: this.google.maps.SymbolPath.CIRCLE, //円を指定
-        //   scale: 16, //円のサイズ
-        //   strokeColor: "#FF0000", //枠の色
-        //   strokeWeight: 1.0, //枠の透過率
+        //   fillColor: "#00FF00", //塗り潰し色
+        //   // fillOpacity: 0.8, //塗り潰し透過率
+        //   // path: "mappin.svg",
+        //   path: "M256,0C160.798,0,83.644,77.155,83.644,172.356c0,97.162,48.158,117.862,101.386,182.495 C248.696,432.161,256,512,256,512s7.304-79.839,70.97-157.148c53.228-64.634,101.386-85.334,101.386-182.495 C428.356,77.155,351.202,0,256,0z M256,231.921c-32.897,0-59.564-26.668-59.564-59.564s26.668-59.564,59.564-59.564 c32.896,0,59.564,26.668,59.564,59.564S288.896,231.921,256,231.921z",
+        //   // path: "M -8,-8 8,8 M 8,-8 -8,8",
+        //   // path: this.google.maps.SymbolPath.FORWARD_CLOSED_ARROW, //円を指定
+        //   // scale: 2, //円のサイズ
+        //   strokeColor: "#0000FF", //枠の色
+        //   // strokeWeight: 1.0, //枠の透過率
         // },
-        label: "99",
+        label: {
+          color: "black" ,
+          fontSize: "12px" ,
+          text: "99" ,
+        },
       });
 
       // マーカー複数配置
