be3c2d8e00d44cf889329322355e5d1b747e1e1c
HiramatsuChihiro <hiramatsu.chr@ncontr.com>
2020-12-04 09:21:28 +0900

    move secret file

---
+++ crudFirestore/src/secrets/firebaseConfig copy.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  projectId: "[PJName]",
+  databaseURL: "https://[PJName].firebaseio.com",
+  apiKey: "[Firebase WebAPI Key]",
+  authDomain: "[PJName].firebaseapp.com",
+  storageBucket: "[PJName].appspot.com",
+};
+
+export default firebaseConfig;
