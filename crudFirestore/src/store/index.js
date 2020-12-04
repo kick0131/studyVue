859b5726f9e2b60e231a7e8008daf721b91615bc
HiramatsuChihiro <hiramatsu.chr@ncontr.com>
2020-12-04 19:08:18 +0900

    Add vuexfire.

--- crudFirestore/src/store/index.js
+++ crudFirestore/src/store/index.js
@@ -1,11 +1,19 @@
 import { createStore } from 'vuex'
+import { firestoreAction, vuexfireMutations } from 'vuexfire';
+import FirestoreDb from '@/common/FirestoreDb';
 
 export default createStore({
   state: {
+    clients: []
   },
   mutations: {
+    // other mutations
+    ...vuexfireMutations
   },
   actions: {
+    bindClients: firestoreAction(({ ref }) => {
+      return ref('clients', FirestoreDb.db.collection('clients'));
+    })
   },
   modules: {
   }
